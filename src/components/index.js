cb98fa98846f11445116aff7771df9bde02d6da9
sourya-6 <saisourya678@gmail.com>
2025-07-29 20:58:14 +0530

    created a mega project with app write as backend service and now created some buttons components

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import container from "./container/container";
+
+
+export {Header,
+    Footer,
+    LogoutBtn,
+    Logo,
+    container
+
+} 
